e14737835bdcac44b6e80f78a3a5d758d85f233d
usuarioo1 <nicolasgonzalezmoller@gmail.com>
2023-12-18 01:01:16 -0300

    fix backend

--- models/funkoSchema.js
+++ models/funkoSchema.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 
 const funkoSchema = new mongoose.Schema({
     nombre: { type: String, require: true },
-    precio: { type: Number, require: true, min: 0, max: 1000000 },
-    img: String,
-    descripcion: String
+    valor: { type: Number, require: true, min: 0, max: 1000000 },
+    descripcion: String,
+    img: String
+    
 });
 
-const Funkos = mongoose.model("funkopop", funkoSchema);
+const Funkos = mongoose.model("funkopops", funkoSchema);
 
 module.exports = Funkos;
--- routes/gamesRoute.js
+++ routes/gamesRoute.js
@@ -3,7 +3,7 @@ const auth = require('../middleware/auth')
 const { getGames, createProduct } = require("../controllers/gamesController");
 const gamesRouter = express.Router();
 
-gamesRouter.route("/games").get(auth,getGames); // la persona que puede ver los jeugos es alguein conectad
+gamesRouter.route("/games").get(getGames); // la persona que puede ver los jeugos es alguein conectad,ademas agregar la auth antes de getgames
 gamesRouter.route("/createProduct").post(createProduct);
 
 module.exports = gamesRouter;
